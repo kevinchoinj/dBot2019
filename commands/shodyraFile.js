8f9095be19d5c6f1a5cd52730ca1c56848912329
unknown <kevinchoinj@yahoo.com>
2020-01-12 21:35:41 -0600

    fileDownload command to download files to local from url

--- commands/shodyraFile.js
+++ commands/shodyraFile.js
@@ -3,8 +3,10 @@ const {
   getRequestValue,
   getRequestValueMulti,
 } = require('../actions/parseData');
+const request = require('request');
 
 const receiveShodyraFileMessage = (message) => {
+  // !fileWrite
   if (message.content.startsWith('!fileWrite')){
     const requestValues = getRequestValueMulti(message.content);
     fs.writeFile(requestValues.valueOne, requestValues.valueTwo, (err) => {
@@ -16,6 +18,7 @@ const receiveShodyraFileMessage = (message) => {
       }
     });
   }
+  // !fileCopy
   else if (message.content.startsWith('!fileCopy')){
     const requestValues = getRequestValueMulti(message.content);
     let myReadStream = fs.createReadStream(requestValues[0], 'utf8');
@@ -23,6 +26,7 @@ const receiveShodyraFileMessage = (message) => {
     myReadStream.pipe(myWriteStream);
     message.channel.send(`File copied: ${requestValues[0]} to ${requestValues[1]}`);
   }
+  // !fileDelete
   else if (message.content.startsWith('!fileDelete')){
     const requestValue = getRequestValue(message.content);
     fs.unlink(requestValue, (err) => {
@@ -34,8 +38,24 @@ const receiveShodyraFileMessage = (message) => {
       }
     });
   }
+  // !fileDownload
+  else if (message.content.startsWith('!fileDownload')){
+    const requestValue = getRequestValue(message.content);
+    const requestArray = requestValue.split('/');
+    const fileName = requestArray[requestArray.length - 1];
+    const stream = function(){
+      request(requestValue)
+        .pipe(fs.createWriteStream(`data/images/${fileName}`))
+        .on('close', () => {
+          message.channel.send(`Finished downloading file: ${fileName}`);
+        })
+        .on('error', (error) =>  {
+          message.channel.send(`Error downloading file: ${requestValue}`);
+        })
+    }
+    stream();
+  }
 };
-
 module.exports = {
   receiveShodyraFileMessage,
 };
